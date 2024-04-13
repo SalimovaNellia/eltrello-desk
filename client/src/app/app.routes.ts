5e9a617510eb6348cac32a2dc01409edacd8e140
SalimovaNellia <salimova.nellia@gmail.com>
2024-04-13 15:17:27 +0200

    feat: add home page murkup

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { RegisterComponent } from './auth/components/register/register.component';
 import { LoginComponent } from './auth/components/login/login.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/components/home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent }
 ];
