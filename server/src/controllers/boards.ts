ba6396da2bc7a5f2214fdfbaebb20252a1d9403a
SalimovaNellia <salimova.nellia@gmail.com>
2024-04-23 16:54:45 +0200

    feat: add fetch board API method

--- server/src/controllers/boards.ts
+++ server/src/controllers/boards.ts
@@ -34,3 +34,16 @@ export const createBoard = async (
         next(err);
     }
 }
+
+export const getBoard = async (
+    req: ExpressRequestInterface,
+    res: Response,
+    next: NextFunction) => {
+    try {
+        if (!req.user) { return res.sendStatus(401); }
+        const board = await BoardModel.findById(req.params.boardId);
+        res.send(board);
+    } catch (err) {
+        next(err);
+    }
+}
